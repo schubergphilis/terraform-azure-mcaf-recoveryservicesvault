ebe644e47f3be2b3846e670c47d32a6526401f34
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2025-01-24 16:38:18 +0100

    fix data block

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "azurerm_user_assigned_identity" "this" {
-  count = var.recovery_service_vault.cmk_identity != null ? 1 : 0
+  count = var.cmk_identity != null ? 1 : 0
 
-  name                = provider::azurerm::parse_resource_id(var.recovery_service_vault.cmk_identity)["resource_name"]
-  resource_group_name = provider::azurerm::parse_resource_id(var.recovery_service_vault.cmk_identity)["resource_group_name"]
+  name                = provider::azurerm::parse_resource_id(var.cmk_identity)["resource_name"]
+  resource_group_name = provider::azurerm::parse_resource_id(var.cmk_identity)["resource_group_name"]
 }
