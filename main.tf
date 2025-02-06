fa999edb8f14ad66aff40f7dc9a1c9178457dd30
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2025-02-06 15:53:18 +0100

    change list items to set and add some initial v1 requirements

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "azurerm_recovery_services_vault" "this" {
 
     content {
       infrastructure_encryption_enabled = true
-      user_assigned_identity_id         = var.system_assigned_identity_enabled == false ? var.cmk_identity : null
+      user_assigned_identity_id         = var.system_assigned_identity_enabled ? null : var.cmk_identity
       use_system_assigned_identity      = var.system_assigned_identity_enabled
       key_id                            = var.cmk_key_vault_key_id
     }
@@ -37,6 +37,54 @@ resource "azurerm_recovery_services_vault" "this" {
     content {
       type         = identity.value.type
       identity_ids = identity.value.user_assigned_resource_ids
+    }
+  }
+}
+
+resource "azurerm_role_assignment" "this" {
+  for_each = var.role_assignments
+
+  principal_id                           = each.value.principal_id
+  scope                                  = azurerm_recovery_services_vault.this.id
+  condition                              = each.value.condition
+  condition_version                      = each.value.condition_version
+  delegated_managed_identity_resource_id = each.value.delegated_managed_identity_resource_id
+  principal_type                         = each.value.principal_type
+  role_definition_name                   = lower(each.value.role_definition_name)
+  skip_service_principal_aad_check       = each.value.skip_service_principal_aad_check
+}
+
+resource "azurerm_monitor_diagnostic_setting" "this" {
+  for_each = var.diagnostic_settings
+
+  name                           = each.value.name != null ? each.value.name : "diag-${var.name}"
+  target_resource_id             = azurerm_recovery_services_vault.this.id
+  eventhub_authorization_rule_id = each.value.event_hub_authorization_rule_resource_id
+  eventhub_name                  = each.value.event_hub_name
+  log_analytics_destination_type = each.value.log_analytics_destination_type
+  log_analytics_workspace_id     = each.value.workspace_resource_id
+  partner_solution_id            = each.value.marketplace_partner_resource_id
+  storage_account_id             = each.value.storage_account_resource_id
+
+  dynamic "enabled_log" {
+    for_each = each.value.log_categories
+
+    content {
+      category = enabled_log.value
+    }
+  }
+  dynamic "enabled_log" {
+    for_each = each.value.log_groups
+
+    content {
+      category_group = enabled_log.value
+    }
+  }
+  dynamic "metric" {
+    for_each = each.value.metric_categories
+
+    content {
+      category = metric.value
     }
   }
 }
