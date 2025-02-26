068b76c6e985d176e2829f79e1eeeed86ba6491f
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2025-02-26 17:15:01 +0100

    var.var

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
-  identity_system_assigned_user_assigned = (var.system_assigned_identity_enabled && (length(var.var.user_assigned_identities) > 0 || var.cmk_identity_id != null)) ? {
+  identity_system_assigned_user_assigned = (var.system_assigned_identity_enabled && (length(var.user_assigned_identities) > 0 || var.cmk_identity_id != null)) ? {
     this = {
       type                       = "SystemAssigned, UserAssigned"
-      user_assigned_resource_ids = setunion(var.var.user_assigned_identities, try([var.cmk_identity_id], []))
+      user_assigned_resource_ids = setunion(var.user_assigned_identities, try([var.cmk_identity_id], []))
     }
   } : null
   identity_system_assigned = var.system_assigned_identity_enabled ? {
@@ -11,10 +11,10 @@ locals {
       user_assigned_resource_ids = null
     }
   } : null
-  identity_user_assigned = (length(var.var.user_assigned_identities) > 0 || var.cmk_identity_id != null) ? {
+  identity_user_assigned = (length(var.user_assigned_identities) > 0 || var.cmk_identity_id != null) ? {
     this = {
       type                       = "UserAssigned"
-      user_assigned_resource_ids = setunion(var.var.user_assigned_identities, try([var.cmk_identity_id], []))
+      user_assigned_resource_ids = setunion(var.user_assigned_identities, try([var.cmk_identity_id], []))
     }
   } : null
 }
